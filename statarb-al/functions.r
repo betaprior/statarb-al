4c0f55b4903c56ff303104e4c788fa5477261828
Leo Alekseyev <leo@matroskin.(none)>
2010-02-10 05:51:08 -0500

    parallelized the s generation from beta, ar (not much gain)

--- statarb-al/functions.r
+++ statarb-al/functions.r
@@ -247,9 +247,9 @@ gen.signals <- function(beta.fit.mtx,ar.fit.mtx,subtract.average, avg.mod=0
 
   ## sig.mtx <- array(dim=c(dim(beta.fit.mtx)[1],num.sig.fields,dim(beta.fit.mtx)[3]))
 
-  cfun <- function(...) abind(...,along=0)
+  cfun <- function(...) abind(...,rev.along=3)
   ## sig.mtx <-
-  foreach(i = 1:dim(beta.fit.mtx)[1], combine = "cfun") %dopar%{
+  foreach(i = 1:(dim(beta.fit.mtx)[1]), .combine = "cfun", .packages="abind") %dopar%{
     m.avg <- mean(ar.fit.mtx[i,1, ,drop=F],na.rm=T)
     if(!subtract.average) m.avg <- avg.mod
     sig.mtx.loc <- 
